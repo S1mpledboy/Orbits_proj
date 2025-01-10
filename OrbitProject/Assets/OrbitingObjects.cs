91bf27526a7bb20b5d824af8e46eb12340bff1f2
S1mpledboy <48122967+S1mpledboy@users.noreply.github.com>
2025-01-10 14:49:53 +0200

    Fixed Bugs
    Now objects properly orbiting

--- OrbitProject/Assets/OrbitingObjects.cs
+++ OrbitProject/Assets/OrbitingObjects.cs
@@ -4,42 +4,43 @@ using UnityEngine;
 
 public class OrbitingObjects : MonoBehaviour
 {
-   [SerializeField] GameObject @object1,@object2,@object3; 
+   [SerializeField] GameObject S1,S2,S3; 
  
 
-    [SerializeField] float rotationSpeedS1 = 50f;   
-    [SerializeField] float rotationSpeedS2 = 30f;
-    [SerializeField] float rotationSpeedS3 = 20f;
-    [SerializeField] float orbitRadiusS2 = 2f;
-    [SerializeField] float orbitRadiusS3 = 1f;
+    [SerializeField] float S1RotationSpeed = 0f;   
+    [SerializeField] float S2OrbitSpeed = 0f;
+    [SerializeField] float S3OrbitSpeed = 0f;
+    [SerializeField] float S2OrbitRadius = 0f;
 
-    private void MoveObjectOnOrbit(GameObject pivot, GameObject orbitingObject, float orbitRadius, float orbitSpeed)
-    { 
-    
-        float angle = orbitSpeed * Time.deltaTime;
-        Matrix4x4 translationMatrix = Matrix4x4.Translate(new Vector3(orbitRadius, 0, 0));
+
+
+    void OrbitObjects(float rotationSpeed, float orbitRadius, GameObject pivot, GameObject orbitingObject)
+    {
+        float angle = rotationSpeed * Time.deltaTime;
         Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 0, angle));
-        Matrix4x4 transformMatrix;
-        if (orbitRadius != 0)
+        Matrix4x4 translationMatrix = Matrix4x4.Translate(new Vector3(orbitRadius, 0, 0));
+        Matrix4x4 transformMatrix = Matrix4x4.zero;
+        if (pivot != null && rotationMatrix != Matrix4x4.zero && translationMatrix!= Matrix4x4.zero)
         {
             transformMatrix = pivot.transform.localToWorldMatrix * rotationMatrix * translationMatrix;
         }
         else
         {
-            transformMatrix = rotationMatrix;
+            orbitingObject.transform.localRotation = Quaternion.LookRotation(Vector3.forward, (orbitingObject.transform.localToWorldMatrix * rotationMatrix).GetColumn(1));
+            return;
         }
-        orbitingObject.transform.position = transformMatrix.GetColumn(3);
-        orbitingObject.transform.rotation = Quaternion.LookRotation(Vector3.forward, transformMatrix.GetColumn(1));
+        orbitingObject.transform.localPosition = transformMatrix.GetColumn(3);
+        orbitingObject.transform.localRotation = Quaternion.LookRotation(Vector3.forward, transformMatrix.GetColumn(1));
+
 
     }
-
-
     private void Update()
     {
-        MoveObjectOnOrbit(object1,object1, 0, rotationSpeedS1);
-        MoveObjectOnOrbit(object1, object2, orbitRadiusS2, rotationSpeedS2);
-        MoveObjectOnOrbit(object2, object3, orbitRadiusS3, rotationSpeedS3);
+        OrbitObjects(S1RotationSpeed, 0f, null, S1);
+        OrbitObjects(S2OrbitSpeed, S2OrbitRadius, S1, S2);
+        OrbitObjects(S3OrbitSpeed, S2OrbitRadius/2, S2, S3);
     }
+
 }
 
 
